d53eb236a4941043a9cccd0299b19c6fb4b476a1
Thomas Lindner <lindner@triumf.ca>
2020-03-28 08:30:20 -0700

    Change readout time

--- altera_load/do_run_enclustra_linux.cmd
+++ altera_load/do_run_enclustra_linux.cmd
@@ -1,7 +1,7 @@
 #!/bin/bash
 xterm -e " nios2_command_shell.sh do_connect_to_jtag_uart1_linux.sh" & 
-echo "sleep 50"
-sleep 50
+echo "sleep 40"
+sleep 40
 xterm -e " nios2_command_shell.sh do_app_nios_load_linux.sh" & 
 echo "sleep 90"
 sleep 90
