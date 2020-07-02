ce65e270dc239325185b2e3a0835cdc3e10911be
Thomas Lindner <lindner@triumf.ca>
2020-07-02 15:28:29 -0700

    Actually fix the polarity swapping bug.

--- analyzer/TBRBRawData.cxx
+++ analyzer/TBRBRawData.cxx
@@ -62,7 +62,7 @@ TBRBRawData::TBRBRawData(int bklen, int bktype, const char* name, void *pdata):
 	int index = i+21+istart; 
 	uint32_t data = (fData[index] >> 4);
 	if(ch == 1 || ch == 3){ data = 4096 - data;}  // Swap polarity of data for channels 1 and 3.
-	Samples[ch].push_back((fData[index] >> 4));	
+	Samples[ch].push_back(data);	
       }
 
 
