cb80cafeaf684c06faa9bef767b3e70741254d2c
Thomas Lindner <lindner@triumf.ca>
2020-08-05 13:53:48 -0700

    Script for setting nominal HV values.

---
+++ hv_control/set_hv.sh
@@ -0,0 +1,25 @@
+odbedit -c "set /Equipment/PMTS/Settings/HVset[0] 1292"
+sleep 2
+odbedit -c "set /Equipment/PMTS/Settings/HVset[1] 1461"
+sleep 2
+#odbedit -c "set /Equipment/PMTS/Settings/HVset[2] 1200"
+sleep 2
+#odbedit -c "set /Equipment/PMTS/Settings/HVset[3] 1200"
+sleep 2
+odbedit -c "set /Equipment/PMTS/Settings/HVset[4] 1352"
+sleep 2
+odbedit -c "set /Equipment/PMTS/Settings/HVset[5] 1460"
+sleep 2
+odbedit -c "set /Equipment/PMTS/Settings/HVset[6] 1287"
+sleep 2
+odbedit -c "set /Equipment/PMTS/Settings/HVset[7] 1418"
+sleep 2
+odbedit -c "set /Equipment/PMTS/Settings/HVset[8] 1354"
+sleep 2
+odbedit -c "set /Equipment/PMTS/Settings/HVset[9] 1346"
+sleep 2
+odbedit -c "set /Equipment/PMTS/Settings/HVset[10] 1474"
+sleep 2
+
+
+
