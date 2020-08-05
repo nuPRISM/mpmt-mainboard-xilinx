be05627f024a274cf5f31c72c59bac4dd91c500d
Thomas Lindner <lindner@triumf.ca>
2020-08-05 16:26:20 -0700

    Crude gain calibration for chans 2 and 3.

--- hv_control/set_hv.sh
+++ hv_control/set_hv.sh
@@ -2,9 +2,9 @@ odbedit -c "set /Equipment/PMTS/Settings/HVset[0] 1292"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[1] 1461"
 sleep 2
-#odbedit -c "set /Equipment/PMTS/Settings/HVset[2] 1200"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[2] 1375"
 sleep 2
-#odbedit -c "set /Equipment/PMTS/Settings/HVset[3] 1200"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[3] 1315"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[4] 1352"
 sleep 2
@@ -14,7 +14,7 @@ odbedit -c "set /Equipment/PMTS/Settings/HVset[6] 1287"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[7] 1418"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[8] 1354"
+#odbedit -c "set /Equipment/PMTS/Settings/HVset[8] 1354"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[9] 1346"
 sleep 2
