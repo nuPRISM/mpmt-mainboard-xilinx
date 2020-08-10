c3af75aa5d6cde339d803879c739eaad754b9444
Thomas Lindner <lindner@triumf.ca>
2020-08-10 16:28:28 -0700

    Set new values.

--- hv_control/set_hv.sh
+++ hv_control/set_hv.sh
@@ -2,19 +2,19 @@ odbedit -c "set /Equipment/PMTS/Settings/HVset[0] 1292"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[1] 1461"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[2] 1375"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[2] 1355"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[3] 1305"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[3] 1285"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[4] 1352"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[5] 1460"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[6] 1287"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[6] 1350"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[7] 1418"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[7] 1428"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[8] 1354"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[8] 1454"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[9] 1346"
 sleep 2
