549122e15de4e010d36f7c67bc7d405b2868d0a5
Thomas Lindner <lindner@triumf.ca>
2020-08-20 11:35:58 -0700

    New HV set points

--- hv_control/set_hv.sh
+++ hv_control/set_hv.sh
@@ -8,7 +8,7 @@ odbedit -c "set /Equipment/PMTS/Settings/HVset[3] 1285"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[4] 1352"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[5] 1460"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[5] 1480"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[6] 1350"
 sleep 2
@@ -20,8 +20,15 @@ odbedit -c "set /Equipment/PMTS/Settings/HVset[9] 1346"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[10] 1474"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[11] 1400"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[11] 1465"
 sleep 2
-
+odbedit -c "set /Equipment/PMTS/Settings/HVset[12] 1360"
+sleep 2
+odbedit -c "set /Equipment/PMTS/Settings/HVset[13] 1380"
+sleep 2
+odbedit -c "set /Equipment/PMTS/Settings/HVset[14] 1401"
+sleep 2
+odbedit -c "set /Equipment/PMTS/Settings/HVset[15] 1429"
+sleep 2
 
 
