00cbd45969afbb6acc2e1c64abd719ab8e2158b4
Thomas Lindner <lindner@triumf.ca>
2020-08-23 17:04:37 -0700

    Update HV setpoints based on longer scan.

--- hv_control/set_hv.sh
+++ hv_control/set_hv.sh
@@ -6,29 +6,29 @@ odbedit -c "set /Equipment/PMTS/Settings/HVset[2] 1355"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[3] 1285"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[4] 1388"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[4] 1390"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[5] 1500"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[5] 1495"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[6] 1350"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[6] 1345"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[7] 1443"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[7] 1438"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[8] 1446"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[8] 1444"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[9] 1365"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[9] 1368"
 sleep 2
 odbedit -c "set /Equipment/PMTS/Settings/HVset[10] 1500"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[11] 1464"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[11] 1454"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[12] 1348"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[12] 1346"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[13] 1375"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[13] 1371"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[14] 1400"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[14] 1401"
 sleep 2
-odbedit -c "set /Equipment/PMTS/Settings/HVset[15] 1424"
+odbedit -c "set /Equipment/PMTS/Settings/HVset[15] 1427"
 sleep 2
 
 
