e93a7ac3fbdc50ba2b90a45e7343736c0b494947
Thomas Lindner <lindner@triumf.ca>
2022-12-07 15:42:25 -0800

    Adding scripts to ramp up and ramp down the BRB06 PMTs

---
+++ hv_control/hv_on_brb06.sh
@@ -0,0 +1,45 @@
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[0] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[1] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[2] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[3] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[4] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[5] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[6] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[7] y"
+sleep 4
+#odbedit -c "set /Equipment/PMTS06/Settings/HVenable[8] y"
+#sleep 4
+#odbedit -c "set /Equipment/PMTS06/Settings/HVenable[9] y"
+#sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[10] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[11] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[12] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[13] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[14] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[15] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[16] y"
+sleep 4
+odbedit -c "set /Equipment/PMTS06/Settings/HVenable[17] y"
+sleep 4
+#odbedit -c "set /Equipment/PMTS06/Settings/HVenable[18] y"
+#sleep 4
+#odbedit -c "set /Equipment/PMTS06/Settings/HVenable[19] y"
+#sleep 4
+
+
+
+
+
